d16c289391f2bb3c1bbc2c0d74e906a430c83159
Kevin Chang <83236234+swekevin@users.noreply.github.com>
2021-05-24 20:16:20 -0600

    Main merge (#25)
    * merging dev to main (#22)
    
    * Renamed build_tables to addColumns, cleaned up commented stuff.
    
    * Removed nodes in UserRepo, added insertTest that's need not be integration test.
    
    * Added a delete() method.
    
    * Improved insert().
    
    * Added some descriptors.
    
    * renaed UserRepo to Repo and deleted AccountRepo
    
    * prepping for a pull
    
    * Revert "Dev"
    
    * Update/chris thomas (#15)
    
    * got partially through the update() method
    
    * foreignkey anno, doubles handled in pstmt, preparePreparedStatement and creatObjectArrayFromResults(), timestamp handled, update() completed
    
    * preparePreparedStatements takes in an ArrayList of relevant fields only based on the number of '?' added, added String[] for id's for update, still need to do it for insert
    
    * merge party and removed extra //TODOs
    
    * deleted UserRepo and AcconutRepo
    
    * Reworked delete().  Needs more testing.
    
    * Fixed delete() with further tests.
    
    * Insert/kevin chang (#21)
    
    * did some cleanup of insert(), still need to return an updated object to user
    
    * Id.class annotation can now target methods (specifically id setter method), insert statement returns object with updated id
    
    * working insert and update method
    
    Co-authored-by: Thomas Diendorf <82783392+gexthegreengecko@users.noreply.github.com>
    Co-authored-by: Gex <gexthegreengecko@protonmail.com>
    
    * fixed some comments
    
    Co-authored-by: Thomas Diendorf <82783392+gexthegreengecko@users.noreply.github.com>
    Co-authored-by: Gex <gexthegreengecko@protonmail.com>

--- src/test/java/InsertTest.java
+++ src/test/java/InsertTest.java
@@ -50,7 +50,8 @@ public class InsertTest {
 
     @Test
 
-    public void insertValuesTest(){
+
+    public void insertValuesTest() throws IllegalAccessException {
         repo.insert(appUser);
         repo.delete(appUser);
 
