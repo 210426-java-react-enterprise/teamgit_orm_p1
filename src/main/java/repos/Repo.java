f44e5f91f7d1bec5d7fa5e8cc1319d26808423e3
Kevin <kevin.chang@revature.net>
2021-05-28 14:36:07 -0600

    removed stack trace

--- src/main/java/repos/Repo.java
+++ src/main/java/repos/Repo.java
@@ -289,7 +289,6 @@ public class Repo {
                     System.out.println("Executing statement: " + pstmt);
                     pstmt.executeUpdate();
                 } catch (SQLException e){
-                    e.printStackTrace();
                     throw new ResourceNotFoundException();
 
                 }
